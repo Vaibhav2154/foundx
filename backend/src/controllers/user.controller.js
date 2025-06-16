25a9961e8094d99deed39056467deaeb05f424f5
Vaibhav2154 <vaibhavvaibhu2005@gmail.com>
2025-06-16 21:44:54 +0530

    Init frontend thanks Yashas urs

---
+++ backend/src/controllers/user.controller.js
@@ -0,0 +1,50 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import ApiError from "../utils/ApiError.js"
+import  User from "../models/user.models.js"
+import  ApiResponse  from "../utils/ApiResponse.js";
+
+
+const registerUser = asyncHandler( async (req, res) => {
+
+
+    const {fullName, email, username, password } = req.body
+    //console.log("email: ", email);
+
+    if (
+        [fullName, email, username, password].some((field) => field?.trim() === "")
+    ) {
+        throw new ApiError(400, "All fields are required")
+    }
+
+    const existedUser = await User.findOne({
+        $or: [{ username }, { email }]
+    })
+
+    if (existedUser) {
+        throw new ApiError(409, "User with email or username already exists")
+    }
+    //console.log(req.files);
+
+   
+
+    const user = await User.create({
+        fullName,
+        email, 
+        password,
+        username: username.toLowerCase()
+    })
+
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+
+    if (!createdUser) {
+        throw new ApiError(500, "Something went wrong while registering the user")
+    }
+
+    return res.status(201).json(
+        new ApiResponse(200, createdUser, "User registered Successfully")
+    )
+
+} )
+export { registerUser };
